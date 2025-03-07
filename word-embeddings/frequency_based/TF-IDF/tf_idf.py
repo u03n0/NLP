2b1b9b7400ee6f270d2780d9a6cf359a659ba6bf
u03n0 <u03n0@proton.me>
2025-03-07 18:41:12 +0100

    added smoothing to idf, and created tf_idf func

--- word-embeddings/frequency_based/TF-IDF/tf_idf.py
+++ word-embeddings/frequency_based/TF-IDF/tf_idf.py
@@ -1,4 +1,7 @@
 import math
+import numpy as np
+from collections import defaultdict
+
 
 
 def tf(d: str, t: str)-> float:
@@ -15,10 +18,30 @@ def tf(d: str, t: str)-> float:
 def idf(D: list[str], t: str)-> float:
     """ Computes the proportion of documents 'd' found in
     a corpus 'D' containing the term 't'.
-    IDF = log ( num of documents in corpus / num of documents having the term)
+    IDF = log ( num of documents in corpus / num of documents having the term).
+    Uses smoothing by adding 1 to numerator, denominator, and log.
     """
-    n_docs: int = len(D)
+    n_docs: int = len(D) # Number of documents in corpus
     n_docs_with_term: int = sum(1 for d in D if t in d)
     if n_docs_with_term == 0:
         return 0
-    return math.log10(n_docs / n_docs_with_term)
+    return math.log(n_docs + 1 / n_docs_with_term + 1) + 1 
+
+
+def tf_idf(D: list[str], V: set[str])-> np.array:
+    """ Calculates the tf-idf of every
+     term in a corpus D.
+    """
+    idfs: dict = {}
+    tfs = defaultdict(dict)
+
+    for idx, d in enumerate(D):
+        for term in V:
+            idfs[term] = idf(D, term) # Store idf of term in corpus
+            tfs[idx][term] = tf(d, term) # Store tf of term in document
+            
+    array = np.zeros((len(D), len(V))) # Initialize array with zeros: shape (|D|, |V|)
+    for i, term in enumerate(V):
+        for j, _ in enumerate(array):
+            array[j][i] = idfs[term] * tfs[j][term] # Fill in array with tf * idf scores
+    return array
