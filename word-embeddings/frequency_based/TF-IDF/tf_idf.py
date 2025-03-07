7ee6fde7246e2da4dfc41453e257d170dc372c67
u03n0 <u03n0@proton.me>
2025-03-07 20:45:40 +0100

    added class

--- word-embeddings/frequency_based/TF-IDF/tf_idf.py
+++ word-embeddings/frequency_based/TF-IDF/tf_idf.py
@@ -45,3 +45,55 @@ def tf_idf(D: list[str], V: set[str])-> np.array:
         for j, _ in enumerate(array):
             array[j][i] = idfs[term] * tfs[j][term] # Fill in array with tf * idf scores
     return array
+
+
+class Tfidf():
+    """ TF-IDF model
+    """
+
+
+    def __init__(self):
+        self.V = None
+        self.D = None
+        self.idfs = {}
+
+
+    def fit_transform(self, D: list[str]):
+        """ Fits and transforms a corpus of texts into
+        a vectorized array.
+        """
+        self.D = D
+        tfs = defaultdict(dict)
+        self.V = {word.lower() for d in self.D for word in d.split()}
+        for idx, d in enumerate(self.D):
+            for term in self.V:
+                self.idfs[term] = self._idf(term)
+                tfs[idx][term] = self._tf(d, term)
+        array = np.zeros((len(self.D), len(self.V)))
+        for i, term in enumerate(self.V):
+            for j, _ in enumerate(array):
+                array[j][i] = self.idfs[term] * tfs[j][term]
+        return array
+    
+
+    def _tf(self, d: str, t: str)-> float:
+        """ Computes the frequency of a term 't' given
+        a document 'd'.
+        TF = term count / number of terms in document.
+        """
+        n_terms: int = len(d.split()) # Amount of terms in 'd'.
+        freq: int = d.count(t) # Frequency of term 't' in 'd'.
+        return freq / n_terms if n_terms else 0.0
+
+    
+    def _idf(self, t: str)-> float:
+        """ Computes the proportion of documents 'd' found in
+        a corpus 'D' containing the term 't'.
+        IDF = log ( num of documents in corpus / num of documents having the term).
+        Uses smoothing by adding 1 to numerator, denominator, and log.
+        """
+        n_docs: int = len(self.D) # Number of documents in corpus
+        n_docs_with_term: int = sum(1 for d in self.D if t in d)
+        if n_docs_with_term == 0:
+            return 0
+        return math.log(n_docs + 1 / n_docs_with_term + 1) + 1 
